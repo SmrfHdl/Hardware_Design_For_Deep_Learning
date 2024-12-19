58ad82e32653490ebabba44ca89737a770957f72
Zuttc <125245597+Blns159@users.noreply.github.com>
2024-12-20 02:14:14 +0700

    Update core.h

--- core.h
+++ core.h
@@ -1,4 +1,3 @@
-// core.h
 #ifndef __CORE_H__
 #define __CORE_H__
 
@@ -9,31 +8,19 @@
 typedef ap_axiu < 8, 2, 5, 6 > uint_8_side_channel;
 typedef ap_axis < 8, 2, 5, 6 > int_8_side_channel;
 
-short convolve2d(hls::Window < KERNEL_DIM, KERNEL_DIM, char > *window,
-                 char kernel[KERNEL_DIM * KERNEL_DIM]);
-
-// Our IP core
-void processImage(hls::stream < uint_8_side_channel >& inStream,
-               hls::stream < int_8_side_channel > & outStream,
-               char kernel[KERNEL_DIM * KERNEL_DIM]);
-
-short sumWindow(hls::Window < KERNEL_DIM, KERNEL_DIM, short > *window);
-short minWindow(hls::Window < KERNEL_DIM, KERNEL_DIM, short > *window);
-short maxWindow(hls::Window < KERNEL_DIM, KERNEL_DIM, short > *window);
-
-void updateLineBuffer(hls::LineBuffer<KERNEL_DIM, IMG_WIDTH, unsigned char>& lineBuff,
-                      unsigned char pixelIn, int idxCol);
-
-void updateWindow(hls::LineBuffer<KERNEL_DIM, IMG_WIDTH, unsigned char>& lineBuff,
-                  hls::Window<KERNEL_DIM, KERNEL_DIM, short>& window,
-                  char kernel[KERNEL_DIM * KERNEL_DIM], int pixConvolved);
-
-short processWindow(hls::Window<KERNEL_DIM, KERNEL_DIM, short>& window, int operation);
-
-void sendOutputData(hls::stream<int_8_side_channel>& outStream,
-                    int_8_side_channel& dataOutSideChannel,
-                    short valOutput,
-                    uint_8_side_channel& currPixelSideChannel,
-                    bool isLast = false);
+void convolution2D(hls::stream<uint_8_side_channel>& inStream,
+                   hls::stream<int_8_side_channel> & outStream,
+                   char kernel[KERNEL_DIM * KERNEL_DIM]);
+void shiftAndInsertLineBuffer(hls::LineBuffer<KERNEL_DIM, IMG_WIDTH, unsigned char>& lineBuff,
+                              unsigned char pixelIn, int idxCol);
+void populateWindowWithKernel(hls::LineBuffer<KERNEL_DIM, IMG_WIDTH, unsigned char>& lineBuff,
+                              hls::Window<KERNEL_DIM, KERNEL_DIM, short>& window,
+                              char kernel[KERNEL_DIM * KERNEL_DIM], int pixConvolved);
+void writeOutputDataStream(hls::stream<int_8_side_channel>& outStream,
+                           int_8_side_channel& dataOutSideChannel,
+                           short valOutput,
+                           uint_8_side_channel& currPixelSideChannel,
+                           bool isLast = false);
+short calculateWindowSum(hls::Window<KERNEL_DIM, KERNEL_DIM, short>* window);
 
 #endif /* __CORE_H__ */
