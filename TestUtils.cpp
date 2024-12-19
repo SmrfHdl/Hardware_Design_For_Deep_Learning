84b3c246bd1f05e682501941f31f92ba8cbd55f2
Zuttc <125245597+Blns159@users.noreply.github.com>
2024-12-20 02:14:52 +0700

    Update TestUtils.cpp

--- TestUtils.cpp
+++ TestUtils.cpp
@@ -1,4 +1,5 @@
 #include "TestUtils.h"
+#include <opencv2/opencv.hpp>
 
 void saveImage(const std::string path, cv::InputArray inArr) {
     double min, max;
@@ -40,27 +41,34 @@ short processWindowAndKernel(short *kernel, char *window, short numKernelElement
 }
 
 void conv2dByHand(cv::InputArray imageIn, char imageOut[IMG_WIDTH][IMG_HEIGHT], char kernel[KERNEL_DIM * KERNEL_DIM], int kSize) {
-    int kCenterX = kSize / 2;
-    int kCenterY = kSize / 2;
-    short accConvolution = 0;
-    short numKernelElements = kSize * kSize;
-    unsigned char samplingWindow[KERNEL_DIM][KERNEL_DIM];
+    cv::Mat inputMat = imageIn.getMat();
+    int rows = inputMat.rows;
+    int cols = inputMat.cols;
 
-    for (int rows = 0; rows < IMG_HEIGHT; rows++) {
-        for (int cols = 0; cols < IMG_HEIGHT; cols++) {
-            accConvolution = 0;
-            if ((rows >= kSize - 1) && (cols >= kSize - 1)) {
-                for (int rowsWindow = 0; rowsWindow < kSize; rowsWindow++) {
-                    for (int colsWindow = 0; colsWindow < kSize; colsWindow++) {
-                        int repos_row = rows - (kSize - 1) + rowsWindow;
-                        int repos_col = cols - (kSize - 1) + colsWindow;
-                        short currPixVal = imageIn.getMat().at<unsigned char>(repos_row, repos_col);
-                        samplingWindow[rowsWindow][colsWindow] = currPixVal;
-                    }
+    cv::Mat outputMat = cv::Mat::zeros(rows, cols, CV_8UC1);
+
+    int kernelRadius = kSize / 2;
+
+    for (int i = kernelRadius; i < rows - kernelRadius; i++) {
+        for (int j = kernelRadius; j < cols - kernelRadius; j++) {
+            int sum = 0;
+
+            for (int m = -kernelRadius; m <= kernelRadius; m++) {
+                for (int n = -kernelRadius; n <= kernelRadius; n++) {
+                    int pixelVal = inputMat.at<uchar>(i + m, j + n);
+                    int kernelVal = kernel[(m + kernelRadius) * kSize + (n + kernelRadius)];
+                    sum += pixelVal * kernelVal;
                 }
-                accConvolution = processWindowAndKernel((short *)kernel, (char *)samplingWindow, numKernelElements);
             }
-            imageOut[rows][cols] = accConvolution;
+
+            sum = std::min(255, std::max(0, sum));
+            outputMat.at<uchar>(i, j) = static_cast<uchar>(sum);
+        }
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            imageOut[i][j] = outputMat.at<uchar>(i, j);
         }
     }
 }
